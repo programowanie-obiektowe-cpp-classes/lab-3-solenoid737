41d49912befbae7249833a8b76c244f54854edf6
Hubert Piwowarski <01150042@pw.edu.pl>
2025-11-05 13:10:52 +0100

    lab3 commit

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,23 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    Stocznia stocznia{};
+    unsigned int transported = 0;
+    int          zaglowiec_counter = 0;
+
+    while (transported < towar)
+    {
+        int single_transport = 0;
+        Statek* s1               = stocznia();
+
+        single_transport = s1->transportuj();
+        transported += single_transport;
+
+        if (Zaglowiec* zaglowiec = dynamic_cast<Zaglowiec*>(s1))
+        {
+            zaglowiec_counter += 1;
+        }
+        delete s1;
+    }
+    return zaglowiec_counter;
 }
